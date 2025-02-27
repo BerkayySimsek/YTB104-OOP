f70e145dbc9f6d31b0e9e7880dc67a4690987501
BerkaySimsek <berkayysimsekk@gmail.com>
2025-02-27 15:41:05 +0300

    static polymorphism commit

---
+++ Polymorphism_2/IMatematik.cs
@@ -0,0 +1,15 @@
+﻿namespace Polymorphism_2;
+
+
+// static Polymorphism : overloading = metodların dönüş tipleri aynı ismi aynı parametre sayısı farklı olan fonksiyonlar.
+public interface IMatematik
+{
+    void Topla(double sayi1, double sayi2);
+    void Topla(double sayi1, double sayi2,double sayi3);
+    void Topla(params double[] numbers);
+    void Carpma(double sayi1, double sayi2);
+    void Carpma(double sayi1, double sayi2,double sayi3);
+    void Carpma(params double[] numbers);
+    void Bolme(double sayi1, double sayi2);
+    void Cikarma(double sayi1, double sayi2);
+}
---
+++ Polymorphism_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Polymorphism_2;
+
+IMatematik matematik = new Matematik();
+
+matematik.Carpma(25, 36,45);
+matematik.Topla(25, 35, 45);
+matematik.Topla(25, 65, 45, 35, 15, 42);
+matematik.Carpma(2, 4, 2, 2);
+matematik.Bolme(100, 0);
